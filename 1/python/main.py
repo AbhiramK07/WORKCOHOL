0613ab140463f9279e998660c58a29f56a37f115
ABHIRAM KANDOORI <137527567+AbhiramK07@users.noreply.github.com>
2025-02-23 11:38:45 +0530

    Add files via upload

---
+++ 1/python/main.py
@@ -0,0 +1,27 @@
+
+# name = "Abhi"
+
+#print(name)
+#print(f"My name is {name}")
+#print("hi, "+ name)
+#print(name[0])
+
+#print(name[0:3])
+
+#print(name[-4:-2])
+
+#count = 1_00_00_000
+#print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
